e7221068ab8d3d3583a1280ec90726963bec58e7
Moses <moses_hamon@bigpond.com>
2019-04-09 17:23:33 +1000

    Added UserOutput component

---
+++ src/User/UserOutput.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class UserOutput extends Component {
+    render(){
+        return (
+            <div>
+                <p>
+                That rewound dauntless pugnaciously much petted while towards before sheep far and dolphin but the mongoose a 
+                cassowary much secure the haphazard crane sourly luxuriant crud out more orca while and ouch against after.
+                </p>
+                <p>
+                Therefore some far derisively glanced darn far filled tentatively far distant realistically far much knitted 
+                this illustratively tactfully hello revealed the quit this broke considering much more and one lobster reckless.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default UserOutput;
