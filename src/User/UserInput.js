8b103c7889f02e59c3e5497fd305074708f1847a
Moses-Hamon <moses_hamon@bigpond.com>
2019-04-09 17:14:49 +1000

    Added input component

---
+++ src/User/UserInput.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class UserInput extends Component {
+
+    render() {
+        return (
+            <div>
+                <label>User Input Field: </label>
+                <input type='text'/>
+            </div>
+        );
+    }
+}
+
+export default UserInput;   
---
+++ src/UserOutput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserOutput {
+    render() {
+        return (
+            <p>
+                Lorem ipsum sollicitudin etiam donec velit at pulvinar sodales donec mollis mi, etiam mi eu nibh aliquet himenaeos cras malesuada convallis platea, potenti massa vulputate ligula eget cras metus turpis viverra eros nam praesent euismod metus nec etiam nibh et consectetur cubilia, ultrices inceptos porta posuere faucibus diam sollicitudin enim curae phasellus, pellentesque id aliquam nisl dictumst a inceptos tempus.
+            </p>
+        );
+    }
+}
+
+export default UserOutput;
