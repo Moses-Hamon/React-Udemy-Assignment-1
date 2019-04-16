fd6eddbe343eb0a7c8eda3d474734bbea5e8bdb3
Moses <moses_hamon@bigpond.com>
2019-04-16 23:48:34 +1000

    Changed to dumb component

--- src/User/UserInput.js
+++ src/User/UserInput.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './UserInput.css';
 
-class UserInput extends Component {
-
-    render() {
+const UserInput = () => {
         return (
             <div className='UserInput'>
                 <label>Please Enter Username: </label>
                 <input type='text' onChange={this.props.changeUser} value={this.props.userName}/>
             </div>
         );
-    }
 }
 
 export default UserInput;   
