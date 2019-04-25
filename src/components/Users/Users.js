6fb9b78cf241c344091a486fcebf3a3d5871a12e
Moses <moses_hamon@bigpond.com>
2019-04-25 16:03:12 +1000

    Converted to Class Component

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import UserOutput from './User/UserOutput';
 
-const users = (props) => props.users.map((user, index) => {
+class Users extends Component {
+  render() {
+    let users = this.props.users.map((user, index) => {
+      return <UserOutput
+        userName={user.name}
+        click={() => this.props.clicked(index)}
+        key={user.id} />
+    });
+
+
+    return (
+      <div>
+        {users}
+      </div>
+    )
+  }
+}
+
+/* const users = (props) => props.users.map((user, index) => {
         return <UserOutput 
         userName={user.name}
         click={() => this.props.clicked(index)}
         key={user.id} />
       });
-
-export default users;
+*/
+export default Users;
